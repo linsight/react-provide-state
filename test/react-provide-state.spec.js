5e05fc6f069bb7327f7b4be50f61d3b3e08beb77
David Lin <davidforest@gmail.com>
2018-02-27 12:15:40 +1100

    Add build assets

---
+++ test/react-provide-state.spec.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+chai.expect();
+
+const expect = chai.expect;
+
+describe('Given an instance of my Cat library', () => {
+  it('should return the name', () => {
+    expect(true).to.be.true;
+  });
+});
